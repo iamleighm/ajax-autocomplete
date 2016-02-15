bfce2552d428f3112b9fbd7895dbb682b064cd9b
Daithi Coombes <webeire@gmail.com>
2016-02-15 21:55:11 +0000

    skeleton js architecture

---
+++ js/cf.autocomplete.js
@@ -0,0 +1,16 @@
+
+// constructor
+function CFAutoComplete(){
+
+  // public
+  this.publicMethod = function(){
+  }
+
+  // private
+  function privateMethod(){
+  }
+}
+
+// static
+CFAutoComplete.prototype.staticMethod = function(){
+}
---
+++ js/global.js
@@ -0,0 +1,9 @@
+// global
+var cfAuto;
+
+
+// when html has loaded.
+$(document).ready(function(){
+
+  cfAuto = new CFAutoComplete()
+});
